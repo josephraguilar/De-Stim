f2c137c86bbeb075db3fe9326722133b8487626e
josephraguilar <josephraguilar@gmail.com>
2018-08-03 15:27:43 -0700

    prettied up code

--- content.js
+++ content.js
@@ -1,21 +1,18 @@
-var settings = new Promise(function (resolve, reject) {
+var settings = new Promise(function(resolve, reject) {
   chrome.storage.sync.get(['fontSize', 'fontColor', 'colorSat'], function(data) {
     let fontSize = (data['fontSize']);
     let fontColor = (data['fontColor']);
     let colorSat = (data['colorSat']);
     resolve([fontSize, fontColor, colorSat]);
   })
-
 })
 
-
-settings.then((e) =>  {
+settings.then((e) => {
   document.body.style.fontSize = e[0] + "px";
   document.body.style.color = e[1];
 
   var imgs = document.getElementsByTagName('img');
-  for (let images of imgs )
-  {
-  images.style.filter = `saturate(${e[2]}%)`;
+  for (let images of imgs) {
+    images.style.filter = `saturate(${e[2]}%)`;
   }
 })
